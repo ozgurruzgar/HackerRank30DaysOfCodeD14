802b16c4ba1c9a788f3480792dca3b15010b9732
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-10 19:43:20 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank30DaysOfCodeD14
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        // Add your code here
+        public Difference(int[] arr)
+        {
+            elements = arr;
+        }
+
+        public void computeDifference()
+        {
+            var min = int.MaxValue;
+            var max = int.MinValue;
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                min = Math.Min(min, elements[i]);
+                max = Math.Max(max, elements[i]);
+            }
+
+            maximumDifference = max - min;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}
